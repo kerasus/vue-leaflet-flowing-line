30954191c9bc1c679489ff31e22f4582d4bc934f
kerasus <aliesmaeeli20@gmail.com>
2020-09-21 14:43:31 +0330

    create index

---
+++ EditablePolyline.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+        <l-polyline
+            :lat-lngs="editablePolylinelatlngs"
+            :color="editablePolylinelatlngsOptions.line.color"
+        >
+        </l-polyline>
+        <l-marker
+            v-if="zoom >= editablePolylinelatlngsOptions.displayZoom"
+            v-for="(item, index) in editablePolylinelatlngs"
+            :lat-lng="item"
+            :draggable="true"
+            @drag="updateEditablePolylineLatLangs($event, item, index)"
+            @dragend="checkForAddPiontToEditablePolylineLatLangs($event, item, index)">
+            <l-icon
+                :icon-url="getEditablePolylineNodeImg(index)"
+                :icon-size="editablePolylinelatlngsOptions.iconSize"
+                :icon-anchor="editablePolylinelatlngsOptions.iconAnchor"
+            >
+                <img :src="getEditablePolylineNodeImg(index)" alt="" />
+            </l-icon>
+        </l-marker>
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import {LMarker, LPolyline, LIcon} from 'vue2-leaflet'
+
+export default {
+    name: "EditablePolyline",
+    props: {
+        latlngs: {
+            type: Array,
+            default: []
+        },
+        zoom: {
+            type: Number,
+            default: 1
+        },
+    },
+    components: {
+        LMarker,
+        LIcon,
+        LPolyline,
+    },
+    data() {
+        return {
+            editablePolylinelatlngs: [],
+            editablePolylinelatlngsOptions: {
+                line: {
+                    color: 'red'
+                },
+                displayZoom: 6,
+                iconSize: [16, 16],
+                iconAnchor: [10, 10]
+            }
+        }
+    },
+    methods: {
+        getEditablePolylineNodeImg(index) {
+            if(index % 2 === 0) {
+                return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAALCAYAAACprHcmAAAAAXNSR0IArs4c6QAAAAZiS0dEAP4ACgAK4wU5iwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAd0SU1FB90EFQswOAsixbAAAAAZdEVYdENvbW1lbnQAQ3JlYXRlZCB3aXRoIEdJTVBXgQ4XAAABDUlEQVQY042QMUoEURBEq3sQ9jeyGm86IpOIycI/gAaCF9gTGHoDzyDew8hMBhSM5ggjbLCZijCDyZ/E32Ww4wauwb6kg6pquloAIIeAYhjgZqcgz+leisgHVJ81pReMiJsBwB7JBwEuGCOkLIGuA5sG0vdLqsYipQ7j5tc8m7mvViRJJzczLxbuITCb7cPNrvJ0Su97/sV9HfP5nD6Z3MFDeMwx/qr8J0Gva2bVd6X7sVTV2ECwhQikqoCiOFBR/WTbrgVy20yCbQvk/I1sdrPTzSHcw80Ocwjc4RsnAgBuVpJcCgDGSClLQdeRTSPS94Dqmab0tGmUQxAA1wJcgjyCyBtFapC3xTB8AcAPM3L4CmzlQRcAAAAASUVORK5CYII=';
+            } else {
+                return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAALCAYAAACprHcmAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH3gEEBgwnW9DvzQAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAEPSURBVBjTbZBBSkJxGMR/89cQnxAKFdQiCTeK0M5c1y08iifwPO46QIgHUJQWlctQ1E1P1N6bFiooNsuZH/N9jDhSGkWX2FdO04KkNSHMQhzPD7n2kGw/Ca7dbKJKBeZz3O+jxeLHIbxl4ngrgCSff6ZUKqjXQ+Uy3rcYcKuFul0svSqNontns4+aTFCxePwVtpGEGw0YDD4D9i212g60T2BJO6/TwZvNXXCaFlStHlLOJKFqFTKZi6AQNh6PD3fPYRuPx5AkabD0zWiEl8uzZts7r91Gudw0CL7YbnG9jieTHXT0QtpqwXCIpffDzpHtFwH/7Awh9EMcz07uJvn8g+AGu4C0tjTF/sisVr8Af3fdgpLllLoZAAAAAElFTkSuQmCC'
+            }
+        },
+        calcCenterLPolylineLatLangs() {
+            let newCenter = JSON.parse(JSON.stringify(this.center));
+            this.editablePolylinelatlngs = [
+                { lat: newCenter.lat-100, lng: newCenter.lng-100},
+                this.calcMiddlePoint({ lat: newCenter.lat-100, lng: newCenter.lng-100}, { lat: newCenter.lat+100, lng: newCenter.lng+100}),
+                { lat: newCenter.lat+100, lng: newCenter.lng+100}
+            ];
+        },
+        calcMiddlePoint(latlang1, latlang2) {
+            return {
+                lat: (latlang1.lat + latlang2.lat)/2,
+                lng: (latlang1.lng + latlang2.lng)/2
+            }
+        },
+        updateEditablePolylineLatLangs(event, item, index) {
+            Vue.set(this.editablePolylinelatlngs, index, event.latlng);
+        },
+        checkForAddPiontToEditablePolylineLatLangs(event, item, index) {
+            if (index % 2 === 0) {
+                return
+            }
+            let latlang1 = this.editablePolylinelatlngs[index-1],
+                latlangIndex = this.editablePolylinelatlngs[index],
+                latlang2 = this.editablePolylinelatlngs[index+1],
+                mid1 = this.calcMiddlePoint(latlang1, latlangIndex),
+                mid2 = this.calcMiddlePoint(latlangIndex, latlang2);
+            this.editablePolylinelatlngs.splice(index, 0, mid1)
+            Vue.set(this.editablePolylinelatlngs, index, mid1);
+            this.editablePolylinelatlngs.splice(index+2, 0, mid2)
+            Vue.set(this.editablePolylinelatlngs, index+2, mid2);
+        }
+    },
+    created: function () {
+        this.editablePolylinelatlngs = this.latlngs;
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
