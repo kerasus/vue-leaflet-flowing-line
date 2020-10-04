7827ecf72c5c1e85d2d6fde67404a0cd55e540a0
kerasus <aliesmaeeli20@gmail.com>
2020-10-04 10:42:58 +0330

    add css and reverse direction

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 'use strict';
 
+import {SVG} from 'leaflet'
 import FlowingLine from "./FlowingLine"
 
-export default FlowingLine
+var FlowingSVG = {
+    install: function (){
+        let updateStyle = SVG.prototype._updateStyle;
+        SVG.prototype._updateStyle = function(layer) {
+            updateStyle(layer);
+            let path1 = layer._path,
+                options2 = layer.options;
+            if (!options2.flowing || !options2.flowing.style) {
+                return;
+            }
+            let styles = '';
+            for (const [key, value] of Object.entries(options2.flowing.style)) {
+                styles += key + ': ' + value + '; ';
+            }
+            styles += '';
+            path1.setAttribute('style', styles);
+            path1.classList.add('leaflet-flowing-line');
+            if (options2.flowing.dir === 'reverse') {
+                path1.classList.add('leaflet-flowing-line__reverse');
+            }
+        };
+    }
+};
+
+export { FlowingLine, FlowingSVG }
